01cc99fb918126f6418bff63614ca34f05069beb
lbojo006 <luisbojorquezgoal@gmail.com>
2025-07-31 14:50:10 -0700

    Workflow Changed

--- src/components/page/navbar.jsx
+++ src/components/page/navbar.jsx
@@ -4,13 +4,17 @@ import HarcLogo from "@/public/images/Harc_Logo.webp";
 const navbar = () => {
   return (
     <div className="flex flex-col">
-      <div className="flex w-full flex-row justify-center gap-5 bg-blue-900 p-5 text-2xl">
-        <p>Option 1</p>
-        <p>Option 2</p>
-        <p>Option 3</p>
-        <p>Option 4</p>
+      <div className="flex w-full flex-row justify-end gap-5 bg-blue-900 p-5 text-2xl">
+        <div>Option 1</div>
+        <div>Option 2</div>
+        <div>Option 3</div>
+        <div>Option 4</div>
       </div>
-      <Image src={HarcLogo} alt="Harc Logo" className="w-1/8" />
+      <Image
+        src={HarcLogo}
+        alt="Harc Logo"
+        className="w-1/4 md:w-1/6 lg:w-1/8"
+      />
     </div>
   );
 };
