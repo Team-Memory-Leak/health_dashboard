43aba2348675a4779f991af149cf8d415c7f3a5e
lbojo006 <luisbojorquezgoal@gmail.com>
2025-08-13 10:14:49 -0700

    Added description data array

---
+++ src/data/Descriptions.ts
@@ -0,0 +1,6 @@
+const descriptions: string[] = [
+  "Every three years, HARC conducts a health survey for the Coachella Valley Community to collect data on the health and well-being of local residents. The data is thoroughly analyzed and shared with the community at no cost. HARC has been helping this community since 2007 and is currently in progress for its seventh edition, the 2025 survey. This website aims to present the 2022 survey findings for individuals aged 18+ through data visualizations that highlight the health status of the Coachella Valley.",
+  "Access to healthcare is a fundamental need for individuals, and health insurance plays a critical role in enabling that need. While nearly all U.S. citizens and permanent residents age 65 and older are covered through Medicare, younger adults face more variability in coverage. This section focuses on healthcare access and exploring trends in health insurance coverage and the disparities that may exist across different populations.",
+];
+
+export default descriptions;
