4d73b4994bb5c740e571f5b3e7f1bcae4834c8f9
lbojo006 <luisbojorquezgoal@gmail.com>
2025-08-19 12:04:22 -0700

    Adjusted Spacing and Background Color

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="font-harc-main flex flex-col items-center bg-gray-50 px-4 py-24 text-center">
+    <div className="font-harc-main flex flex-col items-center px-4 pt-10 pb-24 text-center">
       <h1 className="text-harc-darkblue mb-4 text-6xl font-bold">404</h1>
 
       <p className="text-harc-darkblue mb-8 max-w-2xl text-2xl font-semibold">
