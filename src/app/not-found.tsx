27a09fab1cc58a2cf305d2af41e3321f6cc6d287
lbojo006 <luisbojorquezgoal@gmail.com>
2025-08-19 11:00:58 -0700

    Formatted

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,17 +12,17 @@ export default function NotFound() {
         priority
       />
 
-      <h1 className="font-harc-main mb-4 text-6xl font-bold text-harc-darkblue">
+      <h1 className="font-harc-main text-harc-darkblue mb-4 text-6xl font-bold">
         404
       </h1>
 
-      <p className="font-harc-main mb-8 max-w-2xl text-2xl font-semibold text-harc-darkblue">
+      <p className="font-harc-main text-harc-darkblue mb-8 max-w-2xl text-2xl font-semibold">
         Oops! The page you’re looking for doesn’t exist.
       </p>
 
       <Link
         href="/"
-        className="font-harc-main rounded-lg bg-harc-darkblue px-6 py-3 font-semibold text-white"
+        className="font-harc-main bg-harc-darkblue rounded-lg px-6 py-3 font-semibold text-white"
       >
         Go Back Home
       </Link>
