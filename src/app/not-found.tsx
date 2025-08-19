fe09dbe9cd8b2f6edcaae47af23335fc82bf0768
Ryan Anthony Hoang <mallowford@gmail.com>
2025-08-19 11:39:38 -0700

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,20 @@
 import Link from "next/link";
-import Image from "next/image";
-import Harc_Logo from "@/public/images/Harc_Logo.webp";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center bg-gray-50 px-4 py-24 text-center">
-      <Image
-        src={Harc_Logo}
-        alt="Harc Logo"
-        className="mb-8 h-auto w-32"
-        priority
-      />
+    <div className="flex flex-col items-center bg-gray-50 px-4 py-24 text-center font-harc-main">
 
-      <h1 className="font-harc-main text-harc-darkblue mb-4 text-6xl font-bold">
+      <h1 className="text-harc-darkblue mb-4 text-6xl font-bold">
         404
       </h1>
 
-      <p className="font-harc-main text-harc-darkblue mb-8 max-w-2xl text-2xl font-semibold">
+      <p className="text-harc-darkblue mb-8 max-w-2xl text-2xl font-semibold">
         Oops! The page you’re looking for doesn’t exist.
       </p>
 
       <Link
         href="/"
-        className="font-harc-main bg-harc-darkblue rounded-lg px-6 py-3 font-semibold text-white"
+        className="bg-harc-darkblue rounded-lg px-6 py-3 font-semibold text-white"
       >
         Go Back Home
       </Link>
