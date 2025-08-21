81cea1eb463826c02ec697ebec6456db9ba973e1
Mallowford <mallowford@gmail.com>
2025-08-21 13:59:05 -0700

    Removed Select

--- src/components/Filters/filters.tsx
+++ src/components/Filters/filters.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import { Drawer } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Select } from "@/components/ui/select";
 import { Slider } from "@/components/ui/slider";
 
 const races = [
